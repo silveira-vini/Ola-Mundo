e1ec16c73c0990cae86063ed079fee6b752b61e6
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-17 10:15:50 -0300

    upte17mar-v1.4

---
+++ site-exemplo/scripts/tryCatch.js
@@ -0,0 +1,14 @@
+//tratando erros
+
+try {
+
+console.log(nome);
+
+} catch (err)  {
+    
+    console.log("houve o seguinte erro", `"${err}"`)
+
+} 
+
+console.log("Boa noite, apesar do erro acima")
+
