2ddfaf1b0e9d074889b21d68807b94cc2e4bec0c
Vinicius Silveira <vsribeiro87@gmail.com>
2024-04-04 11:53:47 -0300

    Create ex07.js

---
+++ web-moderno/exercises-js/exerciciosFundContFunc/ex07.js
@@ -0,0 +1,20 @@
+function baskara(a, b, c) {
+    const delta = (b * b) - 4 * (a * c)
+    if (delta < 0) {
+        return 'Delta é negativo, não há raízes reais'
+    } else if (delta == 0) {
+        return `Única raiz real: ${(-b) / (2 * a)}`
+    } else {
+        return `As raízes reais são: ${((-b) + Math.sqrt(delta)) / (2 * a)} e ${((-b) - Math.sqrt(delta)) / (2 * a)}`
+    }
+
+}
+
+// resolvendo 3x² - 5x + 12 = 0
+console.log(baskara(3, -5, 12))
+
+// resolvendo x² - 4x + 4 = 0
+console.log(baskara(1, -4, 4))
+
+// resolvendo 2x² - 5x + 2 = 0
+console.log(baskara(2, -5, 2))
