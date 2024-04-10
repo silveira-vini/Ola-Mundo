9722d15253ce567e3d8cdc607d808b96daf6ad71
Vinicius Silveira <vsribeiro87@gmail.com>
2024-04-10 11:40:06 -0300

    Update ex27.js

--- web-moderno/exercises-js/exerciciosFundContFunc/ex27.js
+++ web-moderno/exercises-js/exerciciosFundContFunc/ex27.js
@@ -1,26 +1,31 @@
-const alturaCrianca1 = 120
-const crescimentoCrianca1 = 1.2
-const alturaCrianca2 = 100
-const crescimentoCrianca2 = 1.15
+function dadosCriancas(alturaCrianca1, alturaCrianca2, txCrianca1, txCrianca2) {
 
-function diferencaAltura(alturaCrianca1, alturaCrianca2) {
+    // Validando os argumentos
+    if (typeof alturaCrianca1 !== 'number' || typeof alturaCrianca2 !== 'number' ||
+        typeof txCrianca1 !== 'number' || typeof txCrianca2 !== 'number' ||
+        alturaCrianca1 < 0 || alturaCrianca2 < 0 || txCrianca1 < 0 || txCrianca2 < 0) {
+        console.error('Os argumentos devem ser números não negativos.');
+        return;
+    }
+
+    // Separando as alturas iguais e diferentes
     if (alturaCrianca1 != alturaCrianca2) {
-        return true
+
+        let tempo = 0
+        while (alturaCrianca1 > alturaCrianca2) {
+            alturaCrianca1 *= txCrianca1
+            alturaCrianca2 *= txCrianca2
+            tempo++
+        }
+        console.log(parseInt(alturaCrianca1), parseInt(alturaCrianca2))
+        console.log(`Em ${tempo} meses passará de altura`)
+
+
     } else {
         console.log('Mesma Altura')
-        return false
     }
 }
 
-diferencaAltura(alturaCrianca1, alturaCrianca2)
 
-function crescimentoCrianca(alturaCrianca1, crescimentoCrianca1, alturaCrianca2, crescimentoCrianca2) {
-    if (alturaCrianca1 > alturaCrianca2) {
-    
-        while (alturaCrianca1 > alturaCrianca2) {
-            alturaCrianca1 *= crescimentoCrianca1
-            alturaCrianca2 *= crescimentoCrianca2
-        }
-    }
+dadosCriancas(120, 100, 1.1, 1.15)
 
-}
