dc262a5461490523817b0a43bb1be276afb97dbd
Vinicius Silveira <vsribeiro87@gmail.com>
2024-04-22 23:29:37 -0300

    Create array.js

---
+++ web-moderno/array/array.js
@@ -0,0 +1,31 @@
+let aprovados = ['bia', 'carlos', 'ana']
+
+console.log(aprovados[0])
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+
+aprovados.push('vinicius')
+
+console.log(aprovados)
+
+console.log(aprovados.length)
+
+aprovados[9] = 'rafael'
+
+console.log(aprovados)
+
+aprovados.sort()
+
+console.log(aprovados)
+
+delete aprovados[5]
+
+console.log(aprovados)
+
+aprovados = ['bia', 'carlos', 'ana']
+
+aprovados.splice(1, 1, 'josé', 'maria')
+
+console.log(aprovados)
+
