8b5cb7f95ea25830b823b750c0653d3ecefc58a8
Vinicius Silveira <vsribeiro87@gmail.com>
2024-04-25 10:19:21 -0300

    Create concat.js

---
+++ web-moderno/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['ualeska', 'cibalena']
+const filhos = ['uoxiton', 'ueskley']
+
+const todos = filhas.concat(filhos, 'fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7], [8,9]]))
